3b5ff3ce8d81f4f88bca796791e4b41d1d42a5b4
ScottN23 <scottn523@gmail.com>
2023-10-17 21:54:34 -0700

    navbar underline/layout

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,37 +1,34 @@
 import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import logo from '../../imgs/Logo.png';
 import './Navbar.css';
 
 export function Navbar(props) {
+    const location = useLocation();
 
     return (
-        <header class="bg-white">
-            <nav class="flex justify-between items-center w-[92%]  mx-auto">
-                <div>
-                    <img class="w-16 cursor-pointer" src={logo} alt="Logo"/>
-                </div>
-                <div
-                    class="nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto  w-full flex items-center px-5">
-                    <ul class="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-12">
+        <header>
+            <nav>
+                <div class="flex-grow">
+                    <ul class="flex flex-row justify-around items-center text-lg">
                         <li>
-                            <a class="hover:text-gray-500" href="/">Home</a>
+                            <img class="w-16" src={logo} alt="Logo"/>
                         </li>
                         <li>
-                            <a class="hover:text-gray-500" href="/Events">Events</a>
+                            <a class={`font-bold ${location.pathname === '/' ? 'border-b-4 border-indigo-500' : ''}`} href="/">Home</a>
                         </li>
                         <li>
-                            <a class="hover:text-gray-500" href="/Resources">Resources</a>
+                            <a class={`font-bold ${location.pathname === '/Events' ? 'border-b-4 border-indigo-500' : ''}`} href="/Events">Events</a>
                         </li>
                         <li>
-                            <a class="hover:text-gray-500" href="/About">About</a>
+                            <a class={`font-bold ${location.pathname === '/Resources' ? 'border-b-4 border-indigo-500' : ''}`} href="/Resources">Resources</a>
+                        </li>
+                        <li>
+                            <a class={`font-bold ${location.pathname === '/About' ? 'border-b-4 border-indigo-500' : ''}`} href="/About">About</a>
                         </li>
                     </ul>
                 </div>
-                <div class="flex items-center gap-6">
-                    <button class="bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec]">Sign in</button>
-                    <ion-icon onclick="onToggleMenu(this)" name="menu" class="text-3xl cursor-pointer md:hidden"></ion-icon>
-                </div>
             </nav>
         </header>
-    )
+    );
 }
