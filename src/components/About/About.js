7ce00d015cc53c37b32cc60fea9865afc021a1ef
ScottN23 <scottn523@gmail.com>
2023-10-18 22:11:02 -0700

    finished home formatting + added matthew

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,39 +11,39 @@ export default function About(props) {
         <div>
             <Navbar />
             <div className="flex flex-col justify-center">
-                <a className="font-bold text-4xl pt-20 px-32"> Meet the team behind Kinguistics</a>
-                <a className="text-xl px-32 pt-6">
+                <p className="font-bold text-4xl pt-20 px-32"> Meet the team behind Kinguistics</p>
+                <p className="text-xl px-32 pt-6">
                     To address the educational disparities faced by underprivileged students in King County schools’
                     foreign language programs, our team aims to support high school students by offering free resources
                     and opportunities for students to further their learning and collaborate with peers outside of school.
-                </a>
+                </p>
                 <div className="flex flex-row flex-wrap justify-around py-6 px-20">
                     <div className="flex flex-col justify-center flex-center">
                         <img className="w-60" src={sarah} alt="Portrait"/>
-                        <a className="font-bold text-2xl">Sarah Thomas<br/></a>
-                        <a className="text-xl">UX/UI Designer + PM</a>
+                        <p className="font-bold text-2xl">Sarah Thomas<br/></p>
+                        <p className="text-xl">UX/UI Designer + PM</p>
                     </div>
                     <div className="flex flex-col justify-center flex-center">
                         <img className="w-60" src={vannary} alt="Portrait"/>
-                        <a className="font-bold text-2xl">Vannary Sou<br/></a>
-                        <a className="text-xl">User Researcher</a>
+                        <p className="font-bold text-2xl">Vannary Sou<br/></p>
+                        <p className="text-xl">User Researcher</p>
                     </div>
                     <div className="flex flex-col justify-center flex-center">
                         <img className="w-60" src={justin} alt="Portrait"/>
-                        <a className="font-bold text-2xl">Justin Sukomol<br/></a>
-                        <a className="text-xl">UX/UI Designer + PM</a>
+                        <p className="font-bold text-2xl">Justin Sukomol<br/></p>
+                        <p className="text-xl">UX/UI Designer + PM</p>
                     </div>
                 </div>
                 <div className="flex flex-row flex-wrap justify-around px-72">
                     <div className="flex flex-col justify-center flex-center">
                         <img className="w-60" src={scott} alt="Portrait"/>
-                        <a className="font-bold text-2xl">Scott Nguyen<br/></a>
-                        <a className="text-xl">Developer</a>
+                        <p className="font-bold text-2xl">Scott Nguyen<br/></p>
+                        <p className="text-xl">Developer</p>
                     </div>
                     <div className="flex flex-col justify-center flex-center">
                         <img className="w-60" src={matthew} alt="Portrait"/>
-                        <a className="font-bold text-2xl">Matthew Bacarro<br/></a>
-                        <a className="text-xl">Developer</a>
+                        <p className="font-bold text-2xl">Matthew Bacarro<br/></p>
+                        <p className="text-xl">Developer</p>
                     </div>
                 </div>
             </div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,43 +14,43 @@ export default function Home(props) {
       <div className="flex flex-col justify-center h-auto">
         <div className="flex flex-col md:flex-row basis-full md:basis-1/2 my-10 mx-32 flex-center">
           <div className="flex flex-col basis-1/2">
-            <a className="md:text-4xl font-bold">Accessible and collaborative language-learning</a>
-            <a className="mt-8 text-xs md:text-2xl">Kinguistics strives to provide accessible collaborative language-learning opportunities for high school students in King County, WA. </a>
+            <p className="md:text-4xl font-bold">Accessible and collaborative language-learning</p>
+            <p className="mt-8 text-xs md:text-2xl">Kinguistics strives to provide accessible collaborative language-learning opportunities for high school students in King County, WA. </p>
           </div>
           <img className="h-fit" src={home} alt="Home" />
         </div>
         <div className="flex flex-col flex-center my-10">
-          <a className="md:text-4xl font-bold">Top Events</a>
-          <a className="text-2xl pt-5">Here’s a selection of our top upcoming events</a>
+          <p className="md:text-4xl font-bold">Top Events</p>
+          <p className="md:text-2xl pt-5">Here’s a selection of our top upcoming events</p>
         </div>
-        <div className="flex flex-row justify-around mx-72">
+        <div className="flex flex-col md:flex-row justify-around mx-72">
           <div className="flex flex-col justify-center flex-center">
-            <img className="h-fit" src={asl} alt="ASL"/>
-            <a className="font-bold text-2xl py-1">ASL Market</a>
-            <a className="text-xl">ASL in Aisles: Sign your way to flavors!</a>
+            <img className="h-fit mb-2" src={asl} alt="ASL"/>
+            <p className="font-bold md:text-2xl py-1">ASL Market</p>
+            <p className="md:text-xl">ASL in Aisles: Sign your way to flavors!</p>
           </div>
           <div className="flex flex-col justify-center flex-center">
-            <img className="h-fit" src={spanish} alt="ASL"/>
-            <a className="font-bold text-2xl py-1">Spanish at the Beach</a>
-            <a className="text-xl">Spanish by the shore: Spice up your getaway!</a>
+            <img className="h-fit mb-2" src={spanish} alt="ASL"/>
+            <p className="font-bold md:text-2xl py-1">Spanish at the Beach</p>
+            <p className="md:text-xl">Spanish by the shore: Spice up your getaway!</p>
           </div>
           <div className="flex flex-col justify-center flex-center">
-            <img className="h-fit" src={french} alt="ASL"/>
-            <a className="font-bold text-2xl py-1">French Bakery</a>
-            <a className="text-xl">Fluent in French: Boss in Bakeries! </a>
+            <img className="h-fit mb-2" src={french} alt="ASL"/>
+            <p className="font-bold md:text-2xl py-1">French Bakery</p>
+            <p className="md:text-xl">Fluent in French: Boss in Bakeries! </p>
           </div>
         </div>
         <div className="flex flex-row justify-center my-12">
-          <Link to="/events" className="bg-indigo-500 text-xl text-white font-bold rounded py-3 px-8">View all Events</Link>
+          <Link to="/events" className="bg-indigo-500 md:text-xl text-white font-bold rounded py-3 px-8">View all Events</Link>
         </div>
         <div className="flex flex-col mx-60 my-10">
-          <a className="md:text-4xl font-bold">How to use Kinguistics</a>
-          <a className="text-2xl pt-5">
+          <p className="md:text-4xl font-bold">How to use Kinguistics</p>
+          <p className="md:text-2xl pt-5">
             Are you a King County, WA high school student looking for additional ways to help you learn and practice a foreign language for class? 
             Here’s how to get started! Follow the steps below to get access to more language learning resources and events:
-          </a>
-          <a className="text-2xl pt-10 pb-3">1. Review the <Link to="/resources" className="underline">Resources</Link> page for more educational tools to support your learning</a>
-          <a className="text-2xl">2. Explore the <Link to="/events" className="underline">Events</Link> page and sign up for upcoming in-person or online events where you can practice your skills with other students.</a>
+          </p>
+          <p className="md:text-2xl pt-10 pb-3">1. Review the <Link to="/resources" className="underline">Resources</Link> page for more educational tools to support your learning</p>
+          <p className="md:text-2xl">2. Explore the <Link to="/events" className="underline">Events</Link> page and sign up for upcoming in-person or online events where you can practice your skills with other students.</p>
         </div>
       </div>
     </div>
