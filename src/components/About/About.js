2bf81048e50c48864f92b9e21bd0460d4af16622
ScottN23 <scottn523@gmail.com>
2023-10-18 19:50:12 -0700

    home/about positioning/siznig

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,37 +12,37 @@ export default function About(props) {
             <Navbar />
             <div className="flex flex-col justify-center">
                 <a className="font-bold text-4xl pt-20 px-32"> Meet the team behind Kinguistics</a>
-                <a className="text-2xl px-32 pt-10">
+                <a className="text-xl px-32 pt-6">
                     To address the educational disparities faced by underprivileged students in King County schools’
                     foreign language programs, our team aims to support high school students by offering free resources
                     and opportunities for students to further their learning and collaborate with peers outside of school.
                 </a>
-                <div className="flex flex-row flex-wrap justify-around py-8 px-20">
+                <div className="flex flex-row flex-wrap justify-around py-6 px-20">
                     <div className="flex flex-col justify-center flex-center">
-                        <img className="h-fit" src={sarah} alt="Portrait"/>
-                        <a className="font-bold text-3xl">Sarah Thomas<br/></a>
+                        <img className="w-60" src={sarah} alt="Portrait"/>
+                        <a className="font-bold text-2xl">Sarah Thomas<br/></a>
                         <a className="text-xl">UX/UI Designer + PM</a>
                     </div>
                     <div className="flex flex-col justify-center flex-center">
-                        <img className="h-fit" src={vannary} alt="Portrait"/>
-                        <a className="font-bold text-3xl">Vannary Sou<br/></a>
+                        <img className="w-60" src={vannary} alt="Portrait"/>
+                        <a className="font-bold text-2xl">Vannary Sou<br/></a>
                         <a className="text-xl">User Researcher</a>
                     </div>
                     <div className="flex flex-col justify-center flex-center">
-                        <img className="h-fit" src={justin} alt="Portrait"/>
-                        <a className="font-bold text-3xl">Justin Sukomol<br/></a>
+                        <img className="w-60" src={justin} alt="Portrait"/>
+                        <a className="font-bold text-2xl">Justin Sukomol<br/></a>
                         <a className="text-xl">UX/UI Designer + PM</a>
                     </div>
                 </div>
-                <div className="flex flex-row flex-wrap justify-around px-32">
+                <div className="flex flex-row flex-wrap justify-around px-72">
                     <div className="flex flex-col justify-center flex-center">
-                        <img className="h-fit" src={scott} alt="Portrait"/>
-                        <a className="font-bold text-3xl">Scott Nguyen<br/></a>
+                        <img className="w-60" src={scott} alt="Portrait"/>
+                        <a className="font-bold text-2xl">Scott Nguyen<br/></a>
                         <a className="text-xl">Developer</a>
                     </div>
                     <div className="flex flex-col justify-center flex-center">
-                        <img className="h-fit" src={matthew} alt="Portrait"/>
-                        <a className="font-bold text-3xl">Matthew Bacarro<br/></a>
+                        <img className="w-60" src={matthew} alt="Portrait"/>
+                        <a className="font-bold text-2xl">Matthew Bacarro<br/></a>
                         <a className="text-xl">Developer</a>
                     </div>
                 </div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,10 +7,10 @@ export default function Home(props) {
   return (
     <div>
       <Navbar />
-      <div className="flex flex-row justify-center pt-10 px-32 h-auto">
-        <div className="flex flex-col justify-center basis-1/2 flex-center">
+      <div className="flex flex-row justify-center pt-10 px-32 h-auto flex-center">
+        <div className="flex flex-col justify-items-start basis-1/2">
           <a className="text-4xl font-bold">Accessible and collaborative language-learning</a>
-          <a className="py-8 text-2xl">Kinguistics strives to provide accessible collaborative language-learning opportunities for high school students in King County, WA. </a>
+          <a className="pt-8 text-2xl">Kinguistics strives to provide accessible collaborative language-learning opportunities for high school students in King County, WA. </a>
         </div>
         <div>
           <img className="h-fit basis-1/2" src={home} alt="Home" />
