ae8fa84cfefb26844de1fc32efc09e414fbc843e
Matthew Bacarro <mbacarro@uw.edu>
2023-10-25 10:49:22 -0700

    changed sign in to login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -10,7 +10,7 @@ export function Login(props) {
 
     return (
         <div>
-            <button className="px-6 py-1 font-bold text-white bg-indigo-500 rounded md:text-xl" onClick={handleSignIn}>Sign In</button>
+            <button className="px-6 py-1 font-bold text-white bg-indigo-500 rounded md:text-xl" onClick={handleSignIn}>Login</button>
         </div>
     );
 }
