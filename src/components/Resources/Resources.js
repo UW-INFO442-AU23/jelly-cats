5419bbdab52d9645ccce61743033437fe08dc1dd
saraht320 <saraht320@gmail.com>
2023-10-26 17:08:55 -0700

    embedded resources video and background

--- src/components/Resources/Resources.js
+++ src/components/Resources/Resources.js
@@ -6,6 +6,17 @@ export default function Resources(props) {
     return (
         <div>
             <Navbar user={props.user} onSignOut={props.onSignOut} />
+            <div className='flex justify-center m-20 rounded'>
+                <iframe width="800" height="395" src="https://www.youtube.com/embed/c0MlOuOPjfU"
+                    title="Why Students Should Learn a Second Language" frameborder="0"
+                    allow="accelerometer; autoplay; fullscreen; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen>
+                </iframe>
+            </div>
+
+            <div className='w-screen h-96 bg-indigo-500 flex justify-center p-16'>
+                <p className='text-lg font-bold md:text-2xl lg:text-4xl text-white'>The Value of Learning a New Language</p>
+            </div>
         </div>
     )
 }
