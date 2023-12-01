fbc4d0190a74abb3c6206f973bc10a2da1908559
Scott Nguyen <scottn523@gmail.com>
2023-11-30 16:24:47 -0800

    updated spacing

--- src/components/Events/EventPage.js
+++ src/components/Events/EventPage.js
@@ -180,44 +180,42 @@ export default function EventPage(props) {
             {/* Event Description + Host Information */}
             <div className="flex flex-col items-center justify-center gap-5 sm:gap-0 sm:justify-between sm:mx-40 sm:mt-20 sm:flex-row">
                 <div className="w-1/2">
-                    <div className="flex flex-col gap-4">
-                        {user ? (
-                            <div>
-                                <div className="flex flex-row items-center justify-between">
-                                    <h2
-                                        className="text-3xl font-bold cursor-pointer"
-                                        onClick={() => handleTabClick('description')}
-                                    >
-                                        Event Description
-                                    </h2>
-                                    <h2
-                                        className="text-3xl font-bold cursor-pointer"
-                                        onClick={() => handleTabClick('flashcards')}
-                                    >
-                                        Flashcards
-                                    </h2>
-                                </div>
-                                <div className="flex flex-row">
-                                    <hr className={`w-1/2 ${activeTab === 'description' ? 'border-indigo-500 border-t-4' : 'border-gray-300 border-t-4'}`}></hr>
-                                    <hr className={`w-1/2 ${activeTab === 'flashcards' ? 'border-indigo-500 border-t-4' : 'border-gray-300 border-t-4'}`}></hr>
-                                </div>
-                                {activeTab === 'description' && (<p className="text-xl">{eventData.Description}</p>)}
-                                {activeTab === 'flashcards' && (
-                                    <Link to={`/Events/${eventName}/Vocabulary`}>
-                                        <div className="flex items-center justify-center">
-                                            <img src={EventFlash} alt="event flashcard navigator" />
-                                        </div>
-                                    </Link>)
-                                }
+                    {user ? (
+                        <div className="flex flex-col gap-4">
+                            <div className="flex flex-row items-center justify-between">
+                                <h2
+                                    className="text-3xl font-bold cursor-pointer"
+                                    onClick={() => handleTabClick('description')}
+                                >
+                                    Event Description
+                                </h2>
+                                <h2
+                                    className="text-3xl font-bold cursor-pointer"
+                                    onClick={() => handleTabClick('flashcards')}
+                                >
+                                    Flashcards
+                                </h2>
                             </div>
-                        ) : (
-                            <div className="flex flex-col gap-4">
-                                <h2 className="text-3xl font-bold"> Event Description</h2>
-                                <hr className="border-t-2 border-gray-300"></hr>
-                                <p className="text-xl">{eventData.Description}</p>
+                            <div className="flex flex-row">
+                                <hr className={`w-1/2 ${activeTab === 'description' ? 'border-indigo-500 border-t-4' : 'border-gray-300 border-t-4'}`}></hr>
+                                <hr className={`w-1/2 ${activeTab === 'flashcards' ? 'border-indigo-500 border-t-4' : 'border-gray-300 border-t-4'}`}></hr>
                             </div>
-                        )}
-                    </div>
+                            {activeTab === 'description' && (<p className="text-xl">{eventData.Description}</p>)}
+                            {activeTab === 'flashcards' && (
+                                <Link to={`/Events/${eventName}/Vocabulary`}>
+                                    <div className="flex items-center justify-center">
+                                        <img src={EventFlash} alt="event flashcard navigator" />
+                                    </div>
+                                </Link>)
+                            }
+                        </div>
+                    ) : (
+                        <div className="flex flex-col gap-4">
+                            <h2 className="text-3xl font-bold"> Event Description</h2>
+                            <hr className="border-t-2 border-gray-300"></hr>
+                            <p className="text-xl">{eventData.Description}</p>
+                        </div>
+                    )}
                 </div>
                 <div className="flex justify-center w-1/2 rounded-xl">
                     <div className="flex flex-col w-3/5 gap-3 text-center bg-white shadow-2xl rounded-2xl sm:gap-0">
